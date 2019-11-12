5d941aea13b76bdb20d9259b76fe27cc57741b0a
xandreax <xandreaxbora@gmail.com>
2019-11-12 11:02:26 +0100

    Test added until number 8

--- test/source/test/roman/DecimalToRomanNumeralTest.java
+++ test/source/test/roman/DecimalToRomanNumeralTest.java
@@ -1,5 +1,39 @@
 package test.roman;
 
+import dssc.roman.RomanNumeral;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DecimalToRomanNumeralTest {
 
+    @Test
+    void zeroIsEmptyString() {
+        check(0, "");
+    }
+
+    @Test
+    void oneIsI(){
+        check(1, "I");
+    }
+
+    @Test
+    void twoISII() {
+        check(2, "II");
+    }
+
+    @Test
+    void fiveIsV() {
+        check(5,"V");
+    }
+
+    @Test
+    void eightIsVIII() {
+        check(8, "VIII");
+    }
+
+    private void check(int decimal, String expected) {
+        RomanNumeral romanNumeral = new RomanNumeral(decimal);
+        assertEquals(expected, romanNumeral.toString());
+    }
 }
